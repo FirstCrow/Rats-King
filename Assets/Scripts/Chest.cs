d379c0031a82cbfb9749136bfa71044af26aa540
Kenneth Shuto <kennethshuto@gmail.com>
2023-05-03 12:21:03 -0700

    Chest and Dialogues are now interactable

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public int amount = 10;
+    [SerializeField] bool open = false;
+    [SerializeField] bool nearPlayer = false;
+    SpriteRenderer mainSprite;
+    public GameObject cheese;
+    public Sprite openChestSprite;
+
+    void Start()
+    {
+        mainSprite = GetComponentInParent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(nearPlayer && !open && Input.GetKeyDown(KeyCode.E))
+        {
+            OpenChest();
+        }
+    }
+
+    void OpenChest()
+    {
+        open = true;
+        mainSprite.sprite = openChestSprite;
+        
+        for(int i = 0; i < amount; i++)
+        {
+            Instantiate(cheese, transform.position + new Vector3(Random.Range(-3f, 3f), Random.Range(-4f, 0f)), Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            nearPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            nearPlayer = false;
+        }
+    }
+}
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class DialogueTrigger : MonoBehaviour
 {
     public bool Repeatable;
+    public bool interactable;
+    [SerializeField] bool nearPlayer = false;
     public GameObject player;
     public GameObject DialogueObject;
 
@@ -29,28 +31,52 @@ public class DialogueTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(interactable && nearPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            StartDialogue();
+        }
+    }
+
+    void StartDialogue()
+    {
+        //Pause Game
+        Time.timeScale = 0;
+
+        DialogueObject.SetActive(true);
+        portrait.SetActive(true);
+        textBox.text = message;
+        portraitImage.sprite = portraitSprite;
+        if (Repeatable == false)
+        {
+            Destroy(gameObject.GetComponent<BoxCollider2D>());
+        }
+        if (!showPortrait)
+        {
+            portrait.SetActive(false);
+            dialogueBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-80, dialogueBox.GetComponent<RectTransform>().anchoredPosition.y);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject == player)
         {
-            //Pause Game
-            Time.timeScale = 0;
-
-            DialogueObject.SetActive(true);
-            portrait.SetActive(true);
-            textBox.text = message;
-            portraitImage.sprite = portraitSprite;
-            if (Repeatable == false) {
-                Destroy(gameObject.GetComponent<BoxCollider2D>());
-            }
-            if (!showPortrait)
+            if (!interactable)
             {
-                portrait.SetActive(false);
-                dialogueBox.GetComponent<RectTransform>().anchoredPosition = new Vector3(-80, dialogueBox.GetComponent<RectTransform>().anchoredPosition.y);
+                StartDialogue();
             }
+            else
+            {
+                nearPlayer = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject == player)
+        {
+            nearPlayer = false;
         }
     }
 }
